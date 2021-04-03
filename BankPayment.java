2eb74aabee6252c6820a90e48a2c2302a567ac06
yogiewes <yogiewisesa@gmail.com>
2021-04-03 09:14:20 +0700

    Modul4 - PostTest

---
+++ BankPayment.java
@@ -0,0 +1,60 @@
+public class BankPayment extends Invoice {
+    
+    private static final PaymentType PAYMENT_TYPE = PaymentType.BankPayment;
+    private int adminFee;
+
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, InvoiceStatus invoiceStatus){
+        super(id, job, date, jobseeker, invoiceStatus);
+        this.adminFee = 0;
+    }
+
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, int adminFee, InvoiceStatus invoiceStatus){
+
+        super(id, job, date, jobseeker, invoiceStatus);
+        this.adminFee = adminFee;
+
+    }
+    
+    public PaymentType getPaymentType(){
+        return PAYMENT_TYPE;
+
+    }
+
+    public int getAdminFee(){
+        return adminFee;
+    }
+
+    public void setBonus(Bonus bonus){
+        this.adminFee = adminFee;
+
+    }
+
+    public void setTotalFee(){
+        if (adminFee != 0){
+            super.totalFee = super.getJob().getFee() - adminFee;
+        } 
+        else 
+        {
+            super.totalFee = super.getJob().getFee();
+        }
+    }
+
+
+    @Override
+    public void printData()
+    {
+        System.out.println("\n==========Invoice==========\n");
+        System.out.println("ID               = "+ super.getId());
+        System.out.println("\nID Job           = "+ super.getJob().getName());
+        System.out.println("\nDate             = "+ super.getDate());
+        System.out.println("\nSeeker           = "+ super.getJobseeker().getName());
+        System.out.println("\nFee              = "+ super.totalFee);
+        
+        System.out.println("\nReferral Code    = "+ getAdminFee());
+        
+        
+        System.out.println("\nStatus           = "+ super.getInvoiceStatus().toString());
+        System.out.println("\nPayment Type     = "+ PAYMENT_TYPE.toString());
+    }
+
+}
--- JWork.java
+++ JWork.java
@@ -14,24 +14,32 @@ public class JWork
             Job job = new Job(6, "Developer", recruiter, 3000, JobCategory.UI);
             Jobseeker jobseeker = new Jobseeker(1, "John Smith", "john.smith@gmail.com", "123", "1 September 2020");
             //Invoice invoice = new Invoice (1, job.getId(), "2 September 2020", job.getFee(), jobseeker, PaymentType.BankPayment, InvoiceStatus.Finished);
-            Bonus bonus1 = new Bonus(2, null, 10, 10, true);
-            Bonus bonus2 = new Bonus(2, "diskongila", 20, 2000000, true);
-            Bonus bonus3 = new Bonus(2, "diskongilaa", 20, 20, true);
-            EwalletPayment ewalletpayment1 = new EwalletPayment(55, job, "2 September 2020", jobseeker, bonus1, InvoiceStatus.Finished);
-            EwalletPayment ewalletpayment2 = new EwalletPayment(66, job, "2 September 2020", jobseeker, bonus2, InvoiceStatus.Finished);
-            EwalletPayment ewalletpayment3 = new EwalletPayment(77, job, "2 September 2020", jobseeker, bonus3, InvoiceStatus.Finished);
+            //Bonus bonus1 = new Bonus(2, null, 10, 10, true);
+            //Bonus bonus2 = new Bonus(2, "diskongila", 20, 2000000, true);
+            //Bonus bonus3 = new Bonus(2, "diskongilaa", 20, 20, true);
+            //EwalletPayment ewalletpayment1 = new EwalletPayment(55, job, "2 September 2020", jobseeker, bonus1, InvoiceStatus.Finished);
+            //EwalletPayment ewalletpayment2 = new EwalletPayment(66, job, "2 September 2020", jobseeker, bonus2, InvoiceStatus.Finished);
+            //EwalletPayment ewalletpayment3 = new EwalletPayment(77, job, "2 September 2020", jobseeker, bonus3, InvoiceStatus.Finished);
 
-            ewalletpayment1.setTotalFee();
-            ewalletpayment2.setTotalFee();
-            ewalletpayment3.setTotalFee();
+            //ewalletpayment1.setTotalFee();
+            //ewalletpayment2.setTotalFee();
+            //ewalletpayment3.setTotalFee();
             
             
             //DatabaseRecruiter.addRecruiter(recruiter);
             //DatabaseJob.addJob(job);
-            ewalletpayment1.printData();
-            ewalletpayment2.printData();
-            ewalletpayment3.printData();
+            //ewalletpayment1.printData();
+            //ewalletpayment2.printData();
+            //ewalletpayment3.printData();
 
+            BankPayment bankPayment1 = new BankPayment(33, job, "2 September 2020", jobseeker, 0, InvoiceStatus.Finished);
+            BankPayment bankPayment2 = new BankPayment(44, job, "2 September 2020", jobseeker, 10, InvoiceStatus.Finished);
+
+            bankPayment1.setTotalFee();
+            bankPayment2.setTotalFee();
+
+            bankPayment1.printData();
+            bankPayment2.printData();
             
     }
     
